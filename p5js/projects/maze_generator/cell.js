a99ee399aced020c2ab6de32080d706d9b48e937
tudyfilp <tudorf90@hotmail.com>
2018-12-07 17:57:08 +0200

    added path finder

--- p5js/projects/maze_generator/cell.js
+++ p5js/projects/maze_generator/cell.js
@@ -6,23 +6,25 @@ function Cell(i, j, scale){
     this.scale = scale;
     this.visited = false;
     this.walls = [true, true, true, true];
+    
+    
 
     this.show = function(){
-        stroke(255);
+        stroke(100);
         if(this.walls[0] === true){
-            line(this.y, this.x, this.y, this.x + this.scale);
+            line(this.x, this.y, this.x, this.y + this.scale);
         }
 
         if(this.walls[1] === true){
-            line(this.y, this.x + this.scale, this.y + this.scale, this.x + this.scale);
+            line(this.x, this.y + this.scale, this.x + this.scale, this.y + this.scale);
         }
 
         if(this.walls[2] === true){
-            line(this.y + this.scale, this.x + this.scale, this.y + this.scale, this.x + this.scale);
+            line(this.x + this.scale, this.y, this.x + this.scale, this.y + this.scale);
         }
 
         if(this.walls[3] === true){
-            line(this.y + this.scale, this.x, this.y, this.x);
+            line(this.x + this.scale, this.y, this.x, this.y);
         }
         fill(0);
         
@@ -31,7 +33,7 @@ function Cell(i, j, scale){
     
 }
 
-Cell.neighbour = function(cells, i, j, rows, cols){
+Cell.randomNeighbour = function(cells, i, j, rows, cols){
     let dl = [-1, 0, 1, 0];
     let dc = [0, 1, 0, -1];
     let neighbours = [];
@@ -44,11 +46,38 @@ Cell.neighbour = function(cells, i, j, rows, cols){
                 neighbours[size++] = cells[lnou][cnou];
     }
 
-   print(neighbours.length);
+   //print(neighbours.length);
    let newCell = neighbours[floor(random()*(neighbours.length))];
    return newCell;
         
 }
+
+Cell.neighbours = function(cells, y, x, rows, cols){
+    let top = {i: y-1, j: x};
+    let right = {i: y, j: x+1};
+    let bottom = {i: y+1, j: x};
+    let left = {i: y, j: x-1};
+
+    cellNeighbours = [];
+
+    if(Cell.insideBounds(top.i, top.j, rows, cols) && !cells[y][x].walls[0])
+        cellNeighbours.push(top);
+    if(Cell.insideBounds(right.i, right.j, rows, cols) && !cells[y][x].walls[1])
+        cellNeighbours.push(right);
+    if(Cell.insideBounds(bottom.i, bottom.j, rows, cols) && !cells[y][x].walls[2])
+        cellNeighbours.push(bottom);
+    if(Cell.insideBounds(left.i, left.j, rows, cols) && !cells[y][x].walls[3])
+        cellNeighbours.push(left);
+
+    // for(let i=0; i<cellNeighbours.length; i++){
+    //     fill(200);
+    //     rect(cellNeighbours[i].y*20, cellNeighbours[i].x*20, 20, 20);
+    // }
+    print(cellNeighbours); 
+    return cellNeighbours;
+        
+}
+
 Cell.insideBounds = function(i, j, rows, cols){
     return (i>=0 && i<rows && j>=0 && j<cols);
 }
--- p5js/projects/maze_generator/sketch.js
+++ p5js/projects/maze_generator/sketch.js
@@ -3,11 +3,14 @@ let current;
 let next;
 let stack = [];
 let visitedCells = 1;
+let mazeSolved = false;
+let parentNode;
+let path;
 function setup() {
   
   frameRate(60);
 
-  board = new Board(20, 40, 40);
+  board = new Board(20, 15, 15);
   
   let cells = [];
 
@@ -25,17 +28,21 @@ function setup() {
   
   current = board.cells[0][0];
   current.visited = true;
-  print(board.cells);
+  //print(board.cells);
 }
 
 function draw() {
   
   background(0);
   fill(0);
-  showCells();
-  fill(200, 0, 0);
-  rect(current.y, current.x, board.scale, board.scale);
-  next = Cell.neighbour(board.cells, current.i, current.j, board.rows, board.cols);
+  
+  fill(255);
+  //rect(4*board.scale, 4*board.scale, board.scale, board.scale);
+  if(!mazeSolved){
+    fill(200, 0, 0);
+    rect(current.x, current.y, board.scale, board.scale);
+  }
+  next = Cell.randomNeighbour(board.cells, current.i, current.j, board.rows, board.cols);
   if(next){
     removeWalls(current, next);
     next.visited = true;
@@ -44,9 +51,18 @@ function draw() {
     visitedCells++;
   } else if(stack.length > 0){
     current = stack.pop();
-  } else 
-    current.x = current.y = 0;
-    
+  } else if(!mazeSolved){
+    solveMaze();
+    // fill(200);
+    // rect(endPath.y, endPath.x, endPath.scale, endPath.scale);
+    mazeSolved = true;
+  } else {
+
+    fill(200, 0, 0);
+    rect(0, 0, board.scale, board.scale);
+    showPath();
+  }
+  showCells();
 }
 function showCells(){
   for(let i=0; i<board.rows; i++)
@@ -57,16 +73,86 @@ function showCells(){
 
 function removeWalls(current, next){
     if(current.i - next.i > 0){
-      current.walls[3] = false;
-      next.walls[1] = false;
-    } else if(current.i - next.i < 0){
-      current.walls[1] = false;
-      next.walls[3] = false;
-    } else if(current.j - next.j < 0){
-      current.walls[2] = false;
-      next.walls[0] = false;
-    } else if(current.j - next.j > 0){
       current.walls[0] = false;
       next.walls[2] = false;
+    } else if(current.i - next.i < 0){
+      current.walls[2] = false;
+      next.walls[0] = false;
+    } else if(current.j - next.j < 0){
+      current.walls[1] = false;
+      next.walls[3] = false;
+    } else if(current.j - next.j > 0){
+      current.walls[3] = false;
+      next.walls[1] = false;
     }
 }
+
+function solveMaze(){
+  let sourceNode = board.cells[0][0];
+
+  let parents = [];
+  let visitedNodes = [];
+  for(let i=0; i<board.rows; i++){
+    visitedNodes[i] = [];
+    parents[i] = [];
+    for(let j=0; j<board.cols; j++){
+      visitedNodes[i][j] = false;
+      parents[i][j] = [0, 0];
+    }
+  }
+  parents[0][0] = [-1, -1];
+  print(parents[0][0]);
+  let contor = 0;
+  let queue = [];
+  queue.push(sourceNode);
+  visitedNodes[sourceNode.i][sourceNode.j] = true;
+  while(queue.length){
+    sourceNode = queue.shift();
+    contor++;
+    //print(queue.length);
+    print(sourceNode);
+    let nodeNeighbours = Cell.neighbours(board.cells,
+      sourceNode.i, sourceNode.j, board.rows, board.cols);
+    //print(nodeNeighbours);
+    for(let j=0; j<nodeNeighbours.length; j++){
+      let newNode = board.cells[nodeNeighbours[j].i][nodeNeighbours[j].j];
+      //print(newNode);
+      if(visitedNodes[newNode.i][newNode.j] == false){
+        
+        visitedNodes[newNode.i][newNode.j] = true;
+        parents[newNode.i][newNode.j] = [sourceNode.i, sourceNode.j];
+        queue.push(newNode);
+        
+      }
+    }
+  }
+  //print(visitedNodes[4][4], contor);
+  parentNode = parents;
+}
+
+function showPath(){
+  let endNode = parentNode[board.rows-1][board.cols-1];
+  color = [];
+  for(let i=0; i<board.rows; i++){
+    color[i] = [];
+    for(let j=0; j<board.cols; j++)
+      color[i][j] = 0;
+  }
+  //print(endNode);
+  color[board.rows-1][board.cols-1] = [200];
+  while(endNode[0]!=-1){
+    color[endNode[0]][endNode[1]] = 200;
+    endNode = parentNode[endNode[0]][endNode[1]];
+  }
+  for(let i=0; i<board.rows; i++)
+    for(let j=0; j<board.cols; j++){
+      //print(color[i][j]);
+      noStroke();
+      fill(color[i][j]);
+      rect(board.cells[i][j].x, board.cells[i][j].y, board.scale, board.scale);
+    }
+      
+}
+
+
+
